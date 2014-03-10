42df613eee828c87dccaeffa09000a4aa5547b08
Sushil Sharma <sushil342@gmail.com>
2014-03-10 19:36:41 +0530

    initialized repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tbook::Application.config.secret_token = '4128a0db4e6d2309fc3acf0c994f56bb136c72c649afcfcc512128c882bc5a4690a4dfc80a8483e34692ff8e0652b3dba1fa42e843b1b6a972594dce92717683'
